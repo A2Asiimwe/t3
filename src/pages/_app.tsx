9250fcc4d6476cea62e542bdd3fd700d0008c423
A2Asiimwe <asiimwearnold65@gmail.com>
2023-03-26 16:56:34 +0300

    Add Auth using [Clerk]

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 
 import { api } from "-/utils/api";
+import { ClerkProvider } from "@clerk/nextjs";
 
 import "-/styles/globals.css";
 
@@ -11,9 +12,14 @@ const MyApp: AppType<{ session: Session | null }> = ({
   pageProps: { session, ...pageProps },
 }) => {
   return (
-    <SessionProvider session={session}>
+    <ClerkProvider {...pageProps}>
+    
+      <SessionProvider session={session}>
       <Component {...pageProps} />
-    </SessionProvider>
+      </SessionProvider>
+     
+    </ClerkProvider>
+    
   );
 };
 
